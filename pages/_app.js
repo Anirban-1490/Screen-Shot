486641942ee4cfda668fb39037f56dd7d9dd9f91
Anirban <anirbanpratihar45@gmail.com>
2023-04-07 11:37:36 +0530

    added navbar and footer component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { Footer } from "Components/Footer/Footer";
+import { Navbar } from "Components/Navbar/Navbar";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import "styles/globals.css";
@@ -6,8 +8,10 @@ export default function App({ Component, pageProps }) {
     const client = new QueryClient();
     return (
         <QueryClientProvider client={client}>
-            <ReactQueryDevtools />
-            <Component {...pageProps} />;
+            {/* <ReactQueryDevtools /> */}
+            <Navbar />
+            <Component {...pageProps} />
+            <Footer />
         </QueryClientProvider>
     );
 }
