bbf97d3816eb1f0efa4bf2b572970f78c4c863ea
Anirban <anirbanpratihar45@gmail.com>
2023-04-07 17:11:34 +0530

    removed protocol from the url, removed some unused codes

--- pages/api/sshot.js
+++ pages/api/sshot.js
@@ -1,18 +1,31 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-    const { url, quality, width, height, pop_up, full_page, device, scale } =
-        req.body;
+    const { url } = req.body;
 
     if (req.method !== "POST")
         return res.status(405).send("Only GET request is allowed");
     if (!url)
         return res.status(400).send("Empty or invaild url. Please try again");
-    const urlObj = new URL("https://website-screenshot-api.exponential.host/");
+
+    let urlWithoutProtocol = url + "";
+
+    //* Check here if the url contains any protocol, if yes then remove it
+    if (urlWithoutProtocol.includes("https://")) {
+        urlWithoutProtocol = urlWithoutProtocol.split("https://")[1];
+    }
+    if (urlWithoutProtocol.includes("http://")) {
+        urlWithoutProtocol = urlWithoutProtocol.split("http://")[1];
+    }
+
+    const urlObj = new URL("https://website-screenshot-api.exponential.host");
+
     for (let [key, value] of Object.entries({
         ...req.body,
         export_format: "jpeg",
+        url: urlWithoutProtocol,
     })) {
+        //*checkbox value check
         if (value) {
             value = value == "on" ? "true" : value;
 
@@ -24,9 +37,11 @@ export default async function handler(req, res) {
         const response = await axios.get(urlObj.href, {
             headers: {
                 exponential_api_secret: process.env.API_KEY,
+                Accept: "image/jpeg",
             },
             responseType: "arraybuffer",
         });
+
         res.status(200).json({ message: "Successful", data: response.data });
     } catch (error) {
         res.status(error.response.status).send(error.message);
